bc5182bb69e8ca574132eceb093b089277941c4b
BhattiBrothers <BhattiBrothers@users.noreply.github.com>
2026-03-07 14:26:20 +0000

    deploy: 0a0fb8a50b717a8fb362078164dc0b8425ccd795

---
+++ ur/assets/js/6ee66e24.27ad0aa1.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[6972],{4945(o){o.exports=JSON.parse('{"metadata":{"permalink":"/physical-ai-textbook/ur/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ ur/assets/js/acecf23e.b410ac71.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[1903],{1912(o){o.exports=JSON.parse('{"blogBasePath":"/physical-ai-textbook/ur/blog","blogTitle":"Blog","authorsListPath":"/physical-ai-textbook/ur/blog/authors"}')}}]);
